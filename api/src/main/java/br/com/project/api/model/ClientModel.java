1a51a5d8d684c4560bd0b64014e73ccff13fbaec
Edvaldo Vitor <edvaldovitor250@.gmail.com>
2024-01-12 12:26:37 -0300

    fixing the database

--- api/src/main/java/br/com/project/api/model/ClientModel.java
+++ api/src/main/java/br/com/project/api/model/ClientModel.java
@@ -23,19 +23,12 @@ public class ClientModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
  private long code;
 
-@NonNull
 private String name;
 
-@NonNull
-@Size(min = 3)
 private int age;
 
-@NonNull
 private String city;
 
-@NotNull(message = "O numero não pode ser nulo.")
-@Size(min = 11, message = "O número deve ter no mínimo 11 caracteres.")
-@Column(unique = true, nullable = false)
 private int number;
     
 }
